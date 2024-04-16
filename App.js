78ae18b399da4ffe9cd012bca84d75e73bc6c7e0
Ayush Raje Chak <ayush20502@iiitd.ac.in>
2024-04-16 12:19:05 +0530

    added safe area view

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, SafeAreaView, View, TouchableOpacity } from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+      <View style={styles.cameraContainer}>
+
+      </View>
+      
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          accessible={true}
+          accessibilityLabel={'Text Mode'}
+          onPress={() => {
+            console.log('Hello')
+          }}
+        >
+          <Ionicons name="book-outline" size={50}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          accessible={true}
+          accessibilityLabel={'Currency Mode'}
+          onPress={() => {
+            console.log('Hello')
+          }}
+        >
+          <Ionicons name="cash-outline" size={50}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          accessible={true}
+          accessibilityLabel={'Walking Mode'}
+          onPress={() => {
+            console.log('Hello')
+          }}
+        >
+          <Ionicons name="walk-outline" size={50}/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          accessible={true}
+          accessibilityLabel={'Face Mode'}
+          onPress={() => {
+            console.log('Hello')
+          }}
+        >
+          <Ionicons name="person-outline" size={50}/>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column",
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly'
   },
+  cameraContainer: {
+    backgroundColor: 'black',
+    height: 500,
+    width: 500
+  },
+  btnContainer: {
+    flexDirection: "row",
+    flex: 0.4,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+
+  }
 });
